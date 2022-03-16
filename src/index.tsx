08b03ffd9987a87e17ad15f1c499fb396ea2d4a5
Len Vo <ailenvo1996@gmail.com>
2022-03-16 12:08:39 +0700

    add service worker

--- src/index.tsx
+++ src/index.tsx
@@ -9,3 +9,18 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+// serviceWorkerRegistration.register({
+//   onUpdate: (sw) => {
+//     if (sw.waiting) {
+//       sw.waiting.postMessage({ type: "SKIP_WAITING" });
+
+//       window.location.reload();
+//       console.log("Update happened.");
+//     }
+//   },
+// });
